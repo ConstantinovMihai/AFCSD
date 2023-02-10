9da06bdf93c0d6995a151709e07207539f2cd386
Mihai Constantinov <97915789+ConstantinovMihai@users.noreply.github.com>
2023-02-10 13:55:19 +0100

    the other pitch rate controller is implemented

--- untitled3.m
+++ untitled3.m
@@ -1,4 +1,4 @@
-rltool(Copy_of_pitchratecommand)
+% rltool(Copy_of_pitchratecommand)
 
 A_red = [  -0.3190      0.9509;
             -0.9356  -0.5072];
@@ -9,16 +9,21 @@ B_red =  [ -0.0007;
 C_red = [57.2958, 0; 0, 57.2958];
 D_red = [0; 0];
 
-sys = ss(A_red, B_red, C_red, D_red);
+sys = ss(A_red, B_red, C_red, D_red);-0.86825
 
 tfs = tf(sys)
-
+% 
 % rltool(-tfs(1))
 
-k1 = -1262.7;
+k1 = -3.0807;
+A_g2 = A_red - B_red * C_red(1,:) * k1
 
-sys2 = ss(A_red + B_red*k1, B_red, C_red, D_red);
+sys2 = ss(A_g2, B_red, C_red, D_red);
 
 tfs2 = tf(sys2);
+k2 = -0.86825
 
-rltool(-tfs(2))
+A_g3 = A_red - B_red * C_red(2,:) * k2
+sys3 = feedback(sys2, [k2, k2]);
+
+damp(sys3)
